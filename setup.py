ea4a5a4c5c411c17d8d77041df922b622b03e653
syth0le <chdan565@gmail.com>
2022-04-30 16:30:39 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py_desc',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/syth0le/py_desc',
+    license='Apache 2.0',
+    author='Cherednichenko Daniil',
+    author_email='chdan565@gmail.com',
+    description='A simple fields descriptors tool'
+)

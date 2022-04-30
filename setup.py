a57af07590cc6dd633dd78b01bb9dce9c797da22
syth0le <chdan565@gmail.com>
2022-04-30 20:59:56 +0300

    updated version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='py_desc',
-    version='0.1.0',
-    packages=['py_desc'],
+    version='0.3.1',
+    packages=['py_desc', 'py_desc/built_in'],
     url='https://github.com/syth0le/py_desc',
     license='Apache 2.0',
     author='Cherednichenko Daniil',

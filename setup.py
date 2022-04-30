2f79e73175284181465ebf82002fb251372ce0a0
syth0le <chdan565@gmail.com>
2022-04-30 21:12:05 +0300

    updated version 2

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='py_desc',
-    version='0.3.1',
+    version='0.3.2',
     packages=['py_desc', 'py_desc/built_in'],
     url='https://github.com/syth0le/py_desc',
     license='Apache 2.0',
     author='Cherednichenko Daniil',
     author_email='syth0le565@gmail.com',
-    description='A simple fields descriptors tool'
+    description='A simple fields descriptors tool',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
 )

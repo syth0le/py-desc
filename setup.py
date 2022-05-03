c0259b43c18eba9a085f6eb547d0fe7912700fa8
syth0le <chdan565@gmail.com>
2022-05-04 00:20:54 +0300

    edited setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     author='Cherednichenko Daniil',
     author_email='syth0le565@gmail.com',
     description='A simple fields descriptors tool',
+    python_requires='>=3.6.0',
+    platforms=['all'],
     classifiers=[
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
@@ -17,5 +20,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries'
     ],
 )

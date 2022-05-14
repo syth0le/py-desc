ed33bb4ee9afb7c740e6a90110049e082d36e94b
syth0le <chdan565@gmail.com>
2022-05-14 11:53:11 +0300

    fixed python version/ upgraded minimal to python 3.7

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@ from setuptools import setup
 
 setup(
     name='py_desc',
-    version='0.4.0',
+    version='0.5.1',
     packages=['py_desc/built_in'],
     url='https://github.com/syth0le/py_desc',
     license='Apache 2.0',
     author='Cherednichenko Daniil',
     author_email='syth0le565@gmail.com',
     description='A simple fields descriptors tool',
-    python_requires='>=3.6.0',
+    python_requires='>=3.7.0',
     platforms=['all'],
     classifiers=[
         'Operating System :: OS Independent',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
